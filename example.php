33ba1f6d30b07765dac55909dd5cae1ea7e3fb44
mhmmdjafarg <mjafarashadiq712@gmail.com>
2021-09-28 00:40:11 +0700

    add more examples on body

--- example.php
+++ example.php
@@ -9,13 +9,22 @@ $pdo = (new SQLiteConnection())->connect();
 if ($pdo != null) {
     echo 'Connected to the SQLite database successfully!';
     $User = new Users($pdo);
-    $newUser = [
-        'email' => 'example@example.com',
-        'username' => 'admin',
-        'password' => 'admin123',
-        'role' => 'admin'
-    ];
-    $User->insertUser($newUser);
-    var_dump($User->getAllUsers());
+    // $newUser = [
+    //     'email' => 'example@example.com',
+    //     'username' => 'example',
+    //     'password' => 'admin123',
+    //     'role' => 'admin'
+    // ];
+    // $User->insertUser($newUser);
+    $userData = $User->getAllUsers();
 } else
     echo 'Whoops, could not connect to the SQLite database!';
+?>
+
+<body>
+    <?php foreach ($userData as $item) {
+        foreach ($item as $val ) { ?>
+            <p><?= $val ?></p>
+    <?php } 
+    } ?>
+</body>
