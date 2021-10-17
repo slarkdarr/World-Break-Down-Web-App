aa51697497c13aad554031d0e9570cf72de2849e
Faziz <farisaziz22@gmail.com>
2021-10-17 13:21:41 +0700

    edit navbar and footer

--- Partials/footer.php
+++ Partials/footer.php
@@ -1,26 +1,8 @@
-<?php
-include_once('config.php');
-include_once('Database/SQLiteConnection.php');
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Assets/css/footer.css">
-    <link rel="stylesheet" href="../Assets/css/style.css">
-    <link href="//db.onlinewebfonts.com/c/4a47df713b67c858644e569c4977de19?family=Dorayaki" rel="stylesheet" type="text/css"/>
-</head>
-<body>
-    <footer>
-        <div class="footer-content">
-            <h3>World Break Down</h3>
-            <p>
-                Situs jajan online terbaik Anda
-            </p>
-        </div>
-    </footer>
-</body>
-</html>
+<footer>
+    <div class="footer-content">
+        <h3>World Break Down</h3>
+        <p>
+            Situs jajan online terbaik Anda
+        </p>
+    </div>
+</footer>
--- Partials/navbar.php
+++ Partials/navbar.php
@@ -1,23 +1,5 @@
-<?php
-include_once('config.php');
-include_once('Database/SQLiteConnection.php');
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Assets/css/navbar.css">
-    <link rel="stylesheet" href="../Assets/css/style.css">
-    <link href="//db.onlinewebfonts.com/c/4a47df713b67c858644e569c4977de19?family=Dorayaki" rel="stylesheet" type="text/css"/>
-</head>
-<body>
-    <ul>
-        <li><a href="/register">Home</a></li>
-        <li><a href="/login">News</a></li>
-        <li style="float:right"><a class="active" href="#about">Login</a></li>
-    </ul>
-</body>
-</html>
+<ul>
+    <li><a href="/register">Home</a></li>
+    <li><a href="/login">News</a></li>
+    <li style="float:right"><a class="active" href="#about">Login</a></li>
+</ul>
