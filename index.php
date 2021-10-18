fe4010c9e17566aa761ca9629edfaee92e721677
mhmmdjafarg <mjafarashadiq712@gmail.com>
2021-10-18 07:24:50 +0700

    refactor import footter and navbar

--- index.php
+++ index.php
@@ -5,10 +5,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="Assets/css/index.css" />
-    <link rel="stylesheet" href="../Assets/css/footer.css">
-    <link rel="stylesheet" href="../Assets/css/navbar.css">
     <link rel="stylesheet" href="../Assets/css/style.css">
+    <link rel="stylesheet" href="Assets/css/index.css" />
     <link href="//db.onlinewebfonts.com/c/4a47df713b67c858644e569c4977de19?family=Dorayaki" rel="stylesheet" type="text/css"/>
     <!-- Font awesome -->
     <script src="https://kit.fontawesome.com/55c10e2ab9.js" crossorigin="anonymous"></script>
--- search.php
+++ search.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="Assets/css/index.css" />
+    <link rel="stylesheet" href="Assets/css/style.css" />
     <!-- Font awesome -->
     <script src="https://kit.fontawesome.com/55c10e2ab9.js" crossorigin="anonymous"></script>
 
