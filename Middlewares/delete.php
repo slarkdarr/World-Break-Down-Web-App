688bf23daf6f02bd3db0499824d90da37836999b
mhmmdjafarg <mjafarashadiq712@gmail.com>
2021-10-21 20:15:41 +0700

    add delete php

---
+++ Middlewares/delete.php
@@ -0,0 +1,37 @@
+<?php
+include_once('../database/SQLiteConnection.php');
+include_once('../Model/Product.php');
+include_once('../config.php');
+
+// Validate logged in
+session_start();
+if (isset($_COOKIE['token']) && isset($_COOKIE['userLoggedIn'])) {
+    if ((md5($_COOKIE['userLoggedIn'] . SECRET_WORD)) !== $_COOKIE['token'] || $_SESSION['role'] !== 'admin') {
+        setcookie('message', 'Prohibited', time() + 3600, '/');
+        header("location: /Views/Login.php");
+    }
+} else {
+    // Destroy session
+    session_unset();
+    session_destroy();
+    setcookie('message', 'Prohibited', time() + 3600, '/');
+    header("location: /Views/Login.php");
+}
+
+if (isset($_GET['id'])) {
+
+    $databasePath = '../database/' . DATABASE_NAME . '.sqlite';
+    $pdo = (new SQLiteConnection())->connect($databasePath);
+    $Product = new Product($pdo);
+    $rowCount = $Product->deleteById($_GET['id']);
+
+    if ($rowCount > 0) {
+        setcookie('message', 'Variant ' . $_GET['id'] . ' deleted successfully', time() + 3600, '/');
+        header("location: /index.php");
+    } else {
+        setcookie('message', 'Variant ' . $_GET['id'] . ' fail to deleted', time() + 3600, '/');
+        header("location: /index.php");
+    }
+} else {
+    header("location: /index.php");
+}
