bc383f4b8b43008c1c66ec719a427935d48d5bf0
Faziz <farisaziz22@gmail.com>
2021-10-22 06:11:46 +0700

    add search bar

--- Partials/navbar.php
+++ Partials/navbar.php
@@ -1,7 +1,15 @@
 <nav>
     <ul>
         <li><a href="/register">Home</a></li>
-        <li><a href="/login">News</a></li>
-        <li style="float:right"><a class="active" href="#logout">Logout</a></li>
+        <li><a href="../Views/LogActivities.php">Log Activities</a></li>
+        <li style="float:right"><a class="active" href="../Middlewares/logout.php">Logout</a></li>
+        <li style="float:right">
+            <div class="search-container">
+                <form action="../search.php" method="GET">
+                <input type="text" id="keyword" name="keyword" placeholder="Variant of product here .." minlength="2" required>
+                <button type="submit" id="search-button">Submit</button>
+                </form>
+            </div>
+        </li>
     </ul>
 </nav>
