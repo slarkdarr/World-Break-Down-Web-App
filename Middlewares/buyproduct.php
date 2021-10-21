4850d28bfc5ada6b17b9ead733877db909c077df
slarkdarr <daffaresyaly@gmail.com>
2021-10-21 23:32:01 +0700

    add real-time price on product buy

--- Middlewares/buyproduct.php
+++ Middlewares/buyproduct.php
@@ -1,6 +1,8 @@
 <?php
 include_once('../database/SQLiteConnection.php');
 include_once('../Model/Product.php');
+include_once('../Model/History.php');
+include_once('../Model/User.php');
 include_once('../config.php');
 
 // Validate logged in
@@ -23,13 +25,28 @@ if (isset($_POST['buy'])) {
     $databasePath = '../database/' . DATABASE_NAME . '.sqlite';
     $pdo = (new SQLiteConnection())->connect($databasePath);
     $Product = new Product($pdo);
+    $History = new History($pdo);
+    $User = new User($pdo);
     $currentProduct = $Product->whereId($_POST['id']);
 
     $newStock = $_POST['stock'];
 
     if ($pdo != null) {
         $bool = $Product->changeStock($newStock, $currentProduct['id']);
+
         if ($bool) {
+            $users = User->whereUsername($_SESSION['username']);
+            $total_price = $currentProduct[3]*$newStock;
+            $history = [
+                'user_id' => $users[0],
+                'username' => $users[2],
+                'product_id' => $currentProduct[0],
+                'product_name' => $currentProduct[1],
+                'quantity'  => $newStock,
+                'total_price' => $currentProduct[0]
+            ];
+            $History->insert();
+            // Set cookie
             setcookie('message', 'Variant ' . $currentProduct['name'] . ' successfully bought', time() + 3600, '/');
             header("location: /index.php");
         } else {
